bc017840c8316583c5dfa13ec552af7fa6618475
dantemayson <meh.far.hdi@gmail.com>
2024-07-30 13:44:41 +0330

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MinverDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
